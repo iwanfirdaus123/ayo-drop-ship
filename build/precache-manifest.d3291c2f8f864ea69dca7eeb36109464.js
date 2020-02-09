0c18a0934125ef0fb47ba8be547aad039c4adff8
Heri Hermawan <herhermawan007@gmail.com>
2020-02-09 12:01:32 +0700

    react-create-app on Heroku

---
+++ build/precache-manifest.d3291c2f8f864ea69dca7eeb36109464.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d0306aacfa60a1a1d1b387a26c2c7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a57eb9f9857e27e97b90",
+    "url": "/static/css/2.3f8f414c.chunk.css"
+  },
+  {
+    "revision": "4bc82069471c35769555",
+    "url": "/static/css/main.941bf783.chunk.css"
+  },
+  {
+    "revision": "a57eb9f9857e27e97b90",
+    "url": "/static/js/2.3000da0a.chunk.js"
+  },
+  {
+    "revision": "4bc82069471c35769555",
+    "url": "/static/js/main.3fafd70a.chunk.js"
+  },
+  {
+    "revision": "c17e49f205f4c8ab2f28",
+    "url": "/static/js/runtime-main.c8df966f.js"
+  },
+  {
+    "revision": "34d752b524abe82365f0769115196b69",
+    "url": "/static/media/divi.34d752b5.png"
+  },
+  {
+    "revision": "3520ceeb01cdfdf480f57edca9538066",
+    "url": "/static/media/facebook.3520ceeb.png"
+  },
+  {
+    "revision": "e033e3db90e4b3b56e0b250c38e7ab6a",
+    "url": "/static/media/logo.e033e3db.svg"
+  },
+  {
+    "revision": "01850828fd542afd90288be64972299f",
+    "url": "/static/media/mail-chimp.01850828.png"
+  },
+  {
+    "revision": "55248adf81038927ac8e84e914d5776a",
+    "url": "/static/media/optimize-press.55248adf.png"
+  },
+  {
+    "revision": "3104f9000e96118ff8bc46972e793b50",
+    "url": "/static/media/paypal.3104f900.png"
+  },
+  {
+    "revision": "400dc88500c0bc5e11f6c953dedcc7ab",
+    "url": "/static/media/twitter.400dc885.png"
+  },
+  {
+    "revision": "20be2fb85597a3553a6b5ffd6cec97a6",
+    "url": "/static/media/wordpress.20be2fb8.png"
+  },
+  {
+    "revision": "1a30986746a7918f9ab1e20ec13bc6e7",
+    "url": "/static/media/zaiper.1a309867.png"
+  }
+]);
